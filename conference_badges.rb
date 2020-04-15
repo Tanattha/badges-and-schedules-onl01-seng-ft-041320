45995baba427f6bcf295b472be33e1d4ca5688a9
Tanattha Thuenim <tanattha.thuenim@gmail.com>
2020-04-15 14:19:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,23 +6,23 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  attendees.map do |attendee|
+  attendees.collect do |attendee|
     "Hello, my name is #{attendee}."
   end
 end
 
 def assign_rooms(attendees)
-  attendees.each_with_index.map do |attendee, index|
+  attendees.each_with_index.collect do |attendee, index|
     "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
   end
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees).each do |badge|
-    puts badge
+  batch_badge_creator(attendees).each do |print_badge|
+    puts print_badge
   end
 
-  assign_rooms(attendees).each do |assignment|
-    puts assignment
+  assign_rooms(attendees).each do |print_assign|
+    puts print_assign
   end
 end
